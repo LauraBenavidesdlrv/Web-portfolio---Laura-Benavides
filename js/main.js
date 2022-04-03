1fb20e8f6c5273bd4fcc383e984c4f938d8cdf11
Laura Benavides <82069691+LauraBenavidesdlrv@users.noreply.github.com>
2022-04-03 08:45:16 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,16 +7,6 @@
 		$banner = $('#banner');
 		
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:    ['1281px',   '1680px'   ],
-			large:     ['981px',    '1280px'   ],
-			medium:    ['737px',    '980px'    ],
-			small:     ['481px',    '736px'    ],
-			xsmall:    ['361px',    '480px'    ],
-			xxsmall:   [null,       '360px'    ]
-		});
-
 	/**
 	 * Applies parallax scrolling to an element's background image.
 	 * @return {jQuery} jQuery object.
