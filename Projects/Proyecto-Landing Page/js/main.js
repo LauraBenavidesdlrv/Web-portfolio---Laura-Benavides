76a775bf58985bf15ce9024509c293e1ecf1bbcb
Laura Benavides <82069691+LauraBenavidesdlrv@users.noreply.github.com>
2022-04-18 16:42:39 -0500

    Update main.js

--- Projects/Proyecto-Landing Page/js/main.js
+++ Projects/Proyecto-Landing Page/js/main.js
@@ -2,7 +2,39 @@
 const caja = document.querySelector(".caja")
 const punto = document.querySelectorAll(".punto")
 
-//Pra pasar
+//form
+function aviso (){
+    let name = document.getElementById("name").value
+    let email = document.getElementById("email").value
+    let user = document.getElementById("user").value
+    let password = document.getElementById("password").value
+    let condicion = document.getElementById("condicion").value
+
+    if (name ==="" ){
+      alert("Debes llenar el campo")
+    }
+    else if (email ==="" ){
+        alert("Debes llenar el campo")
+    }
+    else if (user ==="" ){
+        alert("Debes llenar el campo")
+    }
+    else if (password ==="" ){
+        alert("Debes llenar el campo")
+    }
+    else if (document.getElementById('condicion').checked==false){
+        alert("Debes llenar el campo")
+    }
+    else {
+        alert("Tu registro ha sido exitoso ✅ \n Nombre: "+ name +"\n Email: "+ email + "\n Usuario: "+user)    
+    }
+}
+document.getElementById("registro").onclick = function enviar(){
+    aviso()
+}
+
+
+//Slide
 punto.forEach((cadaUno,i)=> {
     punto[i].addEventListener('click',()=>{
         let ubication = i
